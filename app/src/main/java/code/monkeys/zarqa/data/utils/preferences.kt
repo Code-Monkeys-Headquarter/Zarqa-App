55449ee32d08126d9baaf6345ef4d32177839b45
Yoki Jati Perkasa <yokijati@gmail.com>
2024-07-20 00:49:12 +0700

    Create Preferences for login

---
+++ app/src/main/java/code/monkeys/zarqa/data/utils/preferences.kt
@@ -0,0 +1,28 @@
+package code.monkeys.zarqa.data.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class preferences(context: Context) {
+    private val TAG_STATUS = "status"
+    private val TAG_LEVEL = "level"
+    private val TAG_APP = "app"
+
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(TAG_APP, Context.MODE_PRIVATE)
+
+//    Getter & Setter
+    var prefStatus: Boolean
+        get() = sharedPreferences.getBoolean(TAG_STATUS, false)
+        set(value) =  sharedPreferences.edit().putBoolean(TAG_STATUS, value).apply()
+
+    var prefLevel: String?
+        get() = sharedPreferences.getString(TAG_LEVEL, "")
+        set(value) =  sharedPreferences.edit().putString(TAG_LEVEL, value).apply()
+
+//    Clear
+    fun prefClear() {
+        sharedPreferences.edit().remove(TAG_STATUS).apply()
+        sharedPreferences.edit().remove(TAG_LEVEL).apply()
+    }
+
+}
--- app/src/main/java/code/monkeys/zarqa/data/views/main/MainActivity.kt
+++ app/src/main/java/code/monkeys/zarqa/data/views/main/MainActivity.kt
@@ -1,20 +1,28 @@
-package code.monkeys.zarqa
+package code.monkeys.zarqa.data.views.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import code.monkeys.zarqa.R
+import code.monkeys.zarqa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
